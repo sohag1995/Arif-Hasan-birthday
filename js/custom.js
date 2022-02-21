1dad3a0ee70a3020b6c791639371ef26705952dc
sohag1995 <99829419+sohag1995@users.noreply.github.com>
2022-02-21 11:31:31 +0600

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -14,7 +14,7 @@
             mm = String(today.getMonth() + 1).padStart(2, "0"),
             yyyy = today.getFullYear(),
             nextYear = yyyy + 1,
-            dayMonth = "02/05/",
+            dayMonth = "09/27/",
             birthday = dayMonth + yyyy;
         
         today = mm + "/" + dd + "/" + yyyy;
